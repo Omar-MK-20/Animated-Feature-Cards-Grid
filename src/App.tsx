a5b68486976daea71a4e591604414288955dac6a
Omar-MK-20 <omar.mk.201@gmail.com>
2025-09-30 12:14:56 +0300

    init stage

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Home from './assets/pages/Home'
+
+function App() {
+
+  return (
+      <div className='bg-slate-900 w-full h-screen'>
+        <Home />
+      </div>
+  )
+}
+
+export default App
